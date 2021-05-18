4207da84687b5a6abfc112e78aabf6c1b6eb404a
Giancalabria <calabria.gianluca14@gmail.com>
2021-05-17 21:35:05 -0300

    commit

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -3,15 +3,6 @@ import { useState, React } from 'react'
 import { Cards } from '../cards/cards'
 import './results.css'
 export function Results(props) {
-	function priceCalculator() {
-		for (let i = 1; i <= props.price; i++) {
-			return (
-				<div>
-					<img className='dolar-img' src='./images/price.svg' alt='precio' />
-				</div>
-			)
-		}
-	}
 	return (
 		<div className='results'>
 			{props.newHotelsData.map((objeto, index) => (
